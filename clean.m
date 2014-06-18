091038dcaa6e46998e5b0f30767ca97d61b07942
Jerry Zhu <jerryzhujian9@gmail.com>
2014-06-18 17:30:17 -0500

    separate clean()

---
+++ clean.m
@@ -0,0 +1,7 @@
+% clear command window, all variables. close all figures. show workspace/variables window
+% work at the global workspace
+clc;         % clear command window
+clear all;   % clear workspace/variables
+close all;   % close all figures
+% workspace;   % show/activate workspace/variables window
+commandwindow; % refocus to command window
