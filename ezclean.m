8f7f12fe070b68725e84245fefbe26c4039ec8c5
Jerry Zhu <jerryzhujian9@gmail.com>
2014-07-09 06:06:59 -0500

    ezclean()

--- ezclean.m
+++ ezclean.m
@@ -1,7 +1,9 @@
 % clear command window, all variables. close all figures. show workspace/variables window
 % work at the global workspace
+function ezclean()
 clc;         % clear command window
 clear all;   % clear workspace/variables
 close all;   % close all figures
 % workspace;   % show/activate workspace/variables window
 commandwindow; % refocus to command window
+end
