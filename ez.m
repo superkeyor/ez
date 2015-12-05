90171ada3af69cfaec399e1593a553e4e9f72492
Jerry Zhu <jerryzhujian9@gmail.com>
2015-12-05 11:00:11 -0500

    whichdir

--- ez.m
+++ ez.m
@@ -17,7 +17,7 @@ classdef ez
     %
     %       moment()
     %
-    %       cwd(), pwd(), csd(), csf(), parentdir(path), 
+    %       cwd(), pwd(), csd(), csf(), parentdir(path), whichdir(mfilename)
     %       isdirlike(path), isfilelike(path), 
     %       isdir(path), isfile(path), exists(path)
     %       addpath(path), splitpath(path), joinpath(path1, path2), trimdir(path), cd(path)
@@ -247,6 +247,38 @@ classdef ez
                 result = name;
             catch
                 result = '';
+            end
+        end
+
+        function [dir] = whichdir(name)
+            %   WHICHDIR  Returns directory name containing file in search path.
+            %
+            %   WHICHDIR NAME returns the directory containing a file called NAME,
+            %   i.e. the full path of the file excluding NAME and the trailing '/'. 
+            %
+            %   If more than one directory contains a file called NAME, then 
+            %   WHICHDIR returns a cell array of directories names.
+            %
+            %   If NAME does not include an extension, then it is interpreted as
+            %   the name of an m-file, i.e. having extension '.m' .
+
+            %   copied from:
+            %   _________________________________________________________________
+            %   This is part of JLAB --- type 'help jlab' for more information 
+            %   (C) 2006--2015 J.M. Lilly --- type 'help jlab_license' for details
+              
+            if isempty(strfind(name,'.'))
+                name=[name '.m'];
+            end
+
+            dir=which(name,'-all');
+
+            for i=1:length(dir)
+               dir{i}=dir{i}(1:end-length(name)-1);
+            end
+
+            if i==1
+                dir=dir{1};
             end
         end
 
