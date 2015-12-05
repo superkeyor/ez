94f46165faa335fb34b53f3c97fd54488b3dba3e
Jerry Zhu <jerryzhujian9@gmail.com>
2015-12-05 13:13:47 -0500

    whichdir

--- ez.m
+++ ez.m
@@ -256,13 +256,10 @@ classdef ez
             %   WHICHDIR NAME returns the directory containing a file called NAME,
             %   i.e. the full path of the file excluding NAME and the trailing '/'. 
             %
-            %   If more than one directory contains a file called NAME, then 
-            %   WHICHDIR returns a cell array of directories names.
-            %
             %   If NAME does not include an extension, then it is interpreted as
             %   the name of an m-file, i.e. having extension '.m' .
 
-            %   copied from:
+            %   modified from:
             %   _________________________________________________________________
             %   This is part of JLAB --- type 'help jlab' for more information 
             %   (C) 2006--2015 J.M. Lilly --- type 'help jlab_license' for details
@@ -271,7 +268,10 @@ classdef ez
                 name=[name '.m'];
             end
 
-            dir=which(name,'-all');
+            %   If more than one directory contains a file called NAME, then 
+            %   WHICHDIR returns a cell array of directories names.
+            % dir=which(name,'-all');
+            dir={which(name)};
 
             for i=1:length(dir)
                dir{i}=dir{i}(1:end-length(name)-1);
